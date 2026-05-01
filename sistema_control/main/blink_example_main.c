a39e5c9fffb8e064728fc7413cbf7ad74a4306ab
RubenSah-ing <ruben.sahuquillo.ing@gmail.com>
2026-05-01 20:26:51 +0100

    correcciones estaciones1,2,3 y procesamiento

--- sistema_control/main/blink_example_main.c
+++ sistema_control/main/blink_example_main.c
@@ -1,3 +1,6 @@
+// Autor : Rubén Sahuquillo Redondo
+// Entregable 1 CISTR - Sistema de control de producción de cemento
+
 // Librerias
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
@@ -9,7 +12,6 @@
 #include "led_strip.h"
 #include "sdkconfig.h"
 
-
 // Colas
 QueueHandle_t xQueueCemento;
 QueueHandle_t xQueueAgua;
@@ -19,14 +21,19 @@ QueueHandle_t xQueueEstacion1;
 QueueHandle_t xQueueEstacion2;
 QueueHandle_t xQueueEstacion3;
 
-
 // TAG para debug
 static const char *TAG1 = "UNIDAD DE CARGA";
 static const char *TAG2 = "UNIDAD DE PREPARACION";
 static const char *TAG3 = "UNIDAD DE PROCESAMIENTO";
 static const char *TAG4 = "PANEL DE ESTADO";
 
+// Contador de packs procesados
 static int contador = 0;
+
+// Variables globales para disponibilidad de estaciones
+static int disponible1 = 1;
+static int disponible2 = 1;
+static int disponible3 = 1;
 
 // Variables globales para tiempos
 uint32_t tiempoInicio = 0;
@@ -39,64 +46,47 @@ static uint32_t usStackDepth = 2048;
 TaskHandle_t pvCreatedTask = NULL;
 TaskHandle_t pvParameters = NULL;
 
-
 // Pulsadores de entrada
 #define PULSADOR_CEMENTO GPIO_NUM_16
 #define PULSADOR_AGUA GPIO_NUM_17
 #define PULSADOR_ARENA GPIO_NUM_18
 
 
+
 // Tarea 1: Carga
 void carga(void *pvParameters) {
-    while (1)
-    {
-        int boton_cemento_state = gpio_get_level(PULSADOR_CEMENTO);
-        int boton_agua_state = gpio_get_level(PULSADOR_AGUA);
-        int boton_arena_state = gpio_get_level(PULSADOR_ARENA);
-
-        if (boton_cemento_state == 0)
-        {
+    while (1) {
+        if (gpio_get_level(PULSADOR_CEMENTO) == 0) {
             char valorAEnviar = 1;
-            if (xQueueSend(xQueueCemento, &valorAEnviar, 0) == pdPASS)
-            {
-                int itemsEnCola = uxQueueMessagesWaiting(xQueueCemento);
-                ESP_LOGI(TAG1, ">>> CEMENTO: %d | Elementos en cola: %d", valorAEnviar, itemsEnCola);
+            if (xQueueSend(xQueueCemento, &valorAEnviar, 0) == pdPASS) {
+                ESP_LOGI(TAG1, ">>> CEMENTO cargado");
             }
-            else
-            {
-                ESP_LOGI(TAG1, ">>> Cemento ya disponible");
+            else {
+                ESP_LOGW(TAG1, ">>> Unidad de Carga llena: No se pudo cargar cemento");
             }
+            vTaskDelay(pdMS_TO_TICKS(200));
         }
-
-        else if (boton_agua_state == 0)
-        {
+        else if (gpio_get_level(PULSADOR_AGUA) == 0) {
             char valorAEnviar = 1;
-            if (xQueueSend(xQueueAgua, &valorAEnviar, 0) == pdPASS)
-            {
-                int itemsEnCola = uxQueueMessagesWaiting(xQueueAgua);
-                ESP_LOGI(TAG1, ">>> AGUA: %d | Elementos en cola: %d", valorAEnviar, itemsEnCola);
+            if (xQueueSend(xQueueAgua, &valorAEnviar, 0) == pdPASS) {
+                ESP_LOGI(TAG1, ">>> AGUA cargada");
             }
-            else
-            {
-                ESP_LOGI(TAG1, ">>> Agua ya disponible");
+            else {
+                ESP_LOGW(TAG1, ">>> Unidad de Carga llena: No se pudo cargar agua");
             }
+            vTaskDelay(pdMS_TO_TICKS(200));
         }
-
-        else if (boton_arena_state == 0)
-        {
+        else if (gpio_get_level(PULSADOR_ARENA) == 0) {
             char valorAEnviar = 1;
-            if (xQueueSend(xQueueArena, &valorAEnviar, 0) == pdPASS)
-            {
-                int itemsEnCola = uxQueueMessagesWaiting(xQueueArena);
-                ESP_LOGI(TAG1, ">>> ARENA: %d | Elementos en cola: %d", valorAEnviar, itemsEnCola);
+            if (xQueueSend(xQueueArena, &valorAEnviar, 0) == pdPASS) {
+                ESP_LOGI(TAG1, ">>> ARENA cargada");
             }
-            else
-            {
-                ESP_LOGI(TAG1, ">>> Arena ya disponible");
+            else {
+                ESP_LOGW(TAG1, ">>> Unidad de Carga llena: No se pudo cargar arena");
             }
+            vTaskDelay(pdMS_TO_TICKS(200));
         }
-
-    vTaskDelay(pdMS_TO_TICKS(100)); 
+        vTaskDelay(pdMS_TO_TICKS(50)); 
     }
 }
 
@@ -160,69 +150,88 @@ void procesado(void *pvParameters) {
 
     while (1)
     {
-        xQueueReceive(xQueuePacks, &pack, portMAX_DELAY);
+        if (xQueuePeek(xQueuePacks, &pack, portMAX_DELAY) == pdPASS)
+        {
+            int enviado = 0;
 
-        if (pack == 'W')
-        {
-            xQueueSend(xQueueEstacion1, &valorAEnviar, 0);
-            ESP_LOGI(TAG3, ">>> Pack %c enviado a estación 1", pack);
-        }
-        else if (pack == 'S')
-        {
-            xQueueSend(xQueueEstacion2, &valorAEnviar, 0);
-            ESP_LOGI(TAG3, ">>> Pack %c enviado a estación 2", pack);
-        }
-        else if (pack == 'C')
-        {
-            xQueueSend(xQueueEstacion3, &valorAEnviar, 0);
-            ESP_LOGI(TAG3, ">>> Pack %c enviado a estación 3", pack);
+            if (pack == 'W' && disponible1 == 1)
+            {
+                if (xQueueSend(xQueueEstacion1, &valorAEnviar, 0) == pdPASS)
+                {
+                    xQueueReceive(xQueuePacks, &pack, 0);
+                    ESP_LOGI(TAG3, ">>> Pack %c enviado a estación 1", pack);
+                    enviado = 1;
+                }
+            }
+            else if (pack == 'S' && disponible2 == 1)
+            {
+                if (xQueueSend(xQueueEstacion2, &valorAEnviar, 0) == pdPASS)
+                {
+                    xQueueReceive(xQueuePacks, &pack, 0);
+                    ESP_LOGI(TAG3, ">>> Pack %c enviado a estación 2", pack);
+                    enviado = 1;
+                }
+            }
+            else if (pack == 'C' && disponible3 == 1)
+            {
+                if (xQueueSend(xQueueEstacion3, &valorAEnviar, 0) == pdPASS)
+                {
+                    xQueueReceive(xQueuePacks, &pack, 0);
+                    ESP_LOGI(TAG3, ">>> Pack %c enviado a estación 3", pack);
+                    enviado = 1;
+                }
+            }
         }
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
-
-// Tarea 4: Estacion1
+// Tareas 4, 5 y 6: Estaciones de procesamiento
 void estacion1(void *pvParameters) {
-    while (1)
-    {
-        char paqueteProcesado;
-        xQueueReceive(xQueueEstacion1, &paqueteProcesado, portMAX_DELAY);
-        vTaskDelay(pdMS_TO_TICKS(10000));
-        contador++;
-        tiempoFinEstacion1 = xTaskGetTickCount() * portTICK_PERIOD_MS;
-        int tiempoTranscurrido = tiempoFinEstacion1 - tiempoInicio;
-        ESP_LOGI(TAG3, ">>> PACK PROCESADO en estación 1 [%d] (Total -> %d)", tiempoFinEstacion1, tiempoTranscurrido);
+    char paqueteProcesado;
+    while (1) {
+        if ((xQueuePeek(xQueueEstacion1, &paqueteProcesado, portMAX_DELAY)) && (disponible1 == 1)) {
+            disponible1 = 0;
+            vTaskDelay(pdMS_TO_TICKS(10000)); 
+            xQueueReceive(xQueueEstacion1, &paqueteProcesado, 0); 
+            contador++;
+            uint32_t ahora = xTaskGetTickCount() * portTICK_PERIOD_MS;
+            ESP_LOGI(TAG3, ">>> PACK Estación 1 listo. Total: %u ms", (unsigned int)(ahora - tiempoInicio));
+            disponible1 = 1;
+        }
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
-
-// Tarea 5: Estacion2
 void estacion2(void *pvParameters) {
-    while (1)
-    {
-        char paqueteProcesado;
-        xQueueReceive(xQueueEstacion2, &paqueteProcesado, portMAX_DELAY);
-        vTaskDelay(pdMS_TO_TICKS(10000));
-        contador++;
-        tiempoFinEstacion2 = xTaskGetTickCount() * portTICK_PERIOD_MS;
-        int tiempoTranscurrido = tiempoFinEstacion2 - tiempoInicio;
-        ESP_LOGI(TAG3, ">>> PACK PROCESADO en estación 2 [%d] (Total -> %d)", tiempoFinEstacion2, tiempoTranscurrido);
+    char paqueteProcesado;
+    while (1) {
+        if ((xQueuePeek(xQueueEstacion2, &paqueteProcesado, portMAX_DELAY)) && (disponible2 == 1)) {
+            disponible2 = 0;
+            vTaskDelay(pdMS_TO_TICKS(10000)); 
+            xQueueReceive(xQueueEstacion2, &paqueteProcesado, 0); 
+            contador++;
+            uint32_t ahora = xTaskGetTickCount() * portTICK_PERIOD_MS;
+            ESP_LOGI(TAG3, ">>> PACK Estación 2 listo. Total: %u ms", (unsigned int)(ahora - tiempoInicio));
+            disponible2 = 1;
+        }
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
-
-// Tarea 6: Estacion3
 void estacion3(void *pvParameters) {
-    while (1)
-    {
-        char paqueteProcesado;
-        xQueueReceive(xQueueEstacion3, &paqueteProcesado, portMAX_DELAY);
-        vTaskDelay(pdMS_TO_TICKS(10000));
-        contador++;
-        tiempoFinEstacion3 = xTaskGetTickCount() * portTICK_PERIOD_MS;
-        int tiempoTranscurrido = tiempoFinEstacion3 - tiempoInicio;
-        ESP_LOGI(TAG3, ">>> PACK PROCESADO en estación 3 [%d] (Total -> %d)", tiempoFinEstacion3, tiempoTranscurrido);
+    char paqueteProcesado;
+    while (1) {
+        if ((xQueuePeek(xQueueEstacion3, &paqueteProcesado, portMAX_DELAY)) && (disponible3 == 1)) {
+            disponible3 = 0;
+            vTaskDelay(pdMS_TO_TICKS(10000)); 
+            xQueueReceive(xQueueEstacion3, &paqueteProcesado, 0); 
+            contador++;
+            uint32_t ahora = xTaskGetTickCount() * portTICK_PERIOD_MS;
+            ESP_LOGI(TAG3, ">>> PACK Estación 3 listo. Total: %u ms", (unsigned int)(ahora - tiempoInicio));
+            disponible3 = 1;
+        }
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
@@ -234,6 +243,9 @@ void panelEstado(void *pvParameters) {
         const char *cemento = (uxQueueMessagesWaiting(xQueueCemento) > 0) ? "Yes" : "No";
         const char *agua    = (uxQueueMessagesWaiting(xQueueAgua) > 0)    ? "Yes" : "No";
         const char *arena   = (uxQueueMessagesWaiting(xQueueArena) > 0)   ? "Yes" : "No";
+        const char *cestacion1 = (disponible1 == 1) ? "Disponible" : "Ocupada";
+        const char *cestacion2 = (disponible2 == 1) ? "Disponible" : "Ocupada";
+        const char *cestacion3 = (disponible3 == 1) ? "Disponible" : "Ocupada";
         int packsEnEspera = uxQueueMessagesWaiting(xQueuePacks);
 
         ESP_LOGI(TAG4, 
@@ -242,9 +254,13 @@ void panelEstado(void *pvParameters) {
             "  - Cemento: [%s]\n"
             "  - Agua:    [%s]\n"
             "  - Arena:   [%s]\n"
-            "Packs en preparación: %u\n"
+            "Packs almacenados en preparación: %d\n"
+            "Estado de estaciones de procesamiento:\n"
+            "  - Estación 1: [%s]\n"
+            "  - Estación 2: [%s]\n"
+            "  - Estación 3: [%s]\n"
             "Packs procesados:     %d\n",
-            cemento, agua, arena, packsEnEspera, contador
+            cemento, agua, arena, packsEnEspera, cestacion1, cestacion2, cestacion3, contador
         );
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
@@ -278,13 +294,13 @@ void app_main(void)
     configure_peripherals();
 
     // Colas
-    xQueueCemento = xQueueCreate(1, sizeof(int));
-    xQueueAgua = xQueueCreate(1, sizeof(int));
-    xQueueArena = xQueueCreate(1, sizeof(int));
-    xQueuePacks= xQueueCreate(3, sizeof(int));
-    xQueueEstacion1 = xQueueCreate(1, sizeof(int));
-    xQueueEstacion2 = xQueueCreate(1, sizeof(int));
-    xQueueEstacion3= xQueueCreate(3, sizeof(int));
+    xQueueCemento = xQueueCreate(1, sizeof(char));
+    xQueueAgua = xQueueCreate(1, sizeof(char));
+    xQueueArena = xQueueCreate(1, sizeof(char));
+    xQueuePacks= xQueueCreate(3, sizeof(char));
+    xQueueEstacion1 = xQueueCreate(1, sizeof(char));
+    xQueueEstacion2 = xQueueCreate(1, sizeof(char));
+    xQueueEstacion3= xQueueCreate(1, sizeof(char));
 
     // Tareas
     xTaskCreatePinnedToCore(
